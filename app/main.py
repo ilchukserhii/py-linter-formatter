5ae0f0d9ceb228e0f2521d12ffd63a65439ce48a
Seroga <ilchuk.cergey@gmail.com>
2026-01-06 13:48:06 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,13 +1,81 @@
-def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+error = {
+    "code": "E501",
+    "filename": "./source_code_2.py",
+    "line_number": 18,
+    "column_number": 80,
+    "text": "line too long (99 > 79 characters)",
+    "physical_line": '    return f"I like to filter, rounding, doubling, '
+    "store and decorate numbers: {', '.join(items)}!\"",
+}
 
+def format_linter_error(error: dict) -> dict:
+    return {"line": error["line_number"], "column": error["column_number"], "message": error["text"], "name": error["code"], "source": "flake8"}
+
+print(format_linter_error(error=error))
+errors = [
+    {
+        "code": "E501",
+        "filename": "./source_code_2.py",
+        "line_number": 18,
+        "column_number": 80,
+         "text": "line too long (99 > 79 characters)",
+        "physical_line": '    return f"I like to filter, rounding, doubling, ' 
+        "store and decorate numbers: {', '.join(items)}!\"",
+    },
+    {
+        "code": "W292",
+        "filename": "./source_code_2.py",
+        "line_number": 18,
+        "column_number": 100,
+        "text": "no newline at end of file",
+        "physical_line": '    return f"I like to filter, rounding, doubling, '
+        "store and decorate numbers: {', '.join(items)}!\"",
+    },
+]
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    return {
+            "errors":
+                [{"line": error["line_number"], "column": error["column_number"], "message": error["text"], "name": error["code"], "source": "flake8"}
+                 for error in errors],
+            "path": file_path,
+            "status": ("failed" if errors else "passed")
+            }
+print(format_single_linter_file(file_path="./source_code_2.py", errors=errors))
 
+report_file = {
+    "./test_source_code_2.py": [],
+    "./source_code_2.py":
+        [
+            {
+                "code": "E501",
+                "filename": "./source_code_2.py",
+                "line_number": 18,
+                "column_number": 80,
+                "text": "line too long (99 > 79 characters)",
+                "physical_line": '    return f"I like to filter, rounding, doubling, '
+                "store and decorate numbers: {', '.join(items)}!\"",
+            },
+            {
+                "code": "W292",
+                "filename": "./source_code_2.py",
+                "line_number": 18,
+                "column_number": 100,
+                "text": "no newline at end of file",
+                "physical_line": '    return f"I like to filter, rounding, doubling, '
+                "store and decorate numbers: {', '.join(items)}!\"",
+            },
+        ]
+}
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return [{"errors": (
+            [{"line": error["line_number"], "column": error["column_number"], "message": error["text"],
+              "name": error["code"], "source": "flake8"}
+             for error in errors]
+             if errors else []),
+             "path": file_path,
+             "status": ("failed" if errors else "passed"),}
+             for file_path, errors in linter_report.items()]
+
+print(format_linter_report(linter_report=report_file))
